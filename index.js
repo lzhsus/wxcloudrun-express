fa31e984c22ed3db3d513d0acc1713575218a25c
lzhsus <1434253600@qq.com>
2023-04-20 12:15:50 +0800

    测试

--- index.js
+++ index.js
@@ -49,13 +49,23 @@ app.post('/api/apply-car-qr', async (req, res) => {
     name:"龙张海",
     openID:req.headers['x-wx-openid']
   }
-  await ApplyCarQrUserInfo.create(data);
+  ApplyCarQrUserInfo.create(data);
   res.send({
     code: 0,
     data: data,
   });
 });
-
+app.post('/api/apply-car-qr2', async (req, res) => {
+  let data = {
+    mobile:15239371927,
+    name:"龙张海",
+    openID:req.headers['x-wx-openid']
+  }
+  res.send({
+    code: 0,
+    data: data,
+  });
+});
 
 
 // 小程序调用，获取微信 Open ID
